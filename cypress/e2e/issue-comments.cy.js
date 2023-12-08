a529354d72017789295388addc4a44f067cbdb55
IamLaaith <vitali.shapoval@gmail.com>
2023-12-08 16:19:27 +0100

    Uploading Assingment 1 and 2. issue-comments.cy.js and issue-time-tracking.cy.js

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -9,7 +9,7 @@ describe('Issue comments creating, editing and deleting', () => {
 
     const getIssueDetailsModal = () => cy.get('[data-testid="modal:issue-details"]');
 
-    it('Should create a comment successfully', () => {
+    it.skip('Should create a comment successfully', () => {
         const comment = 'TEST_COMMENT';
 
         getIssueDetailsModal().within(() => {
@@ -17,7 +17,7 @@ describe('Issue comments creating, editing and deleting', () => {
                 .click();
 
             cy.get('textarea[placeholder="Add a comment..."]').type(comment);
-
+            
             cy.contains('button', 'Save')
                 .click()
                 .should('not.exist');
@@ -27,7 +27,7 @@ describe('Issue comments creating, editing and deleting', () => {
         });
     });
 
-    it('Should edit a comment successfully', () => {
+    it.skip('Should edit a comment successfully', () => {
         const previousComment = 'An old silent pond...';
         const comment = 'TEST_COMMENT_EDITED';
 
@@ -53,7 +53,7 @@ describe('Issue comments creating, editing and deleting', () => {
         });
     });
 
-    it('Should delete a comment successfully', () => {
+    it.skip('Should delete a comment successfully', () => {
         getIssueDetailsModal()
             .find('[data-testid="issue-comment"]')
             .contains('Delete')
@@ -68,4 +68,66 @@ describe('Issue comments creating, editing and deleting', () => {
             .find('[data-testid="issue-comment"]')
             .should('not.exist');
     });
+
+    it.only('Assignment 1. Test Combination: Comments Functionality. One comprehensive tets.', () => {
+        const comment = 'TEST_COMMENT';
+        const previousComment = comment;
+        const editComment = 'TEST_COMMENT_EDITED';
+
+
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...')
+                .click();
+
+            cy.get('textarea[placeholder="Add a comment..."]').type(comment);
+            
+            
+            cy.contains('button', 'Save')
+                .click()
+            // This line is for testing if it's my mac that has a problem
+            //  .click({ x: 20, y: 20 })
+                .should('not.exist');
+
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', comment);
+        });
+
+        getIssueDetailsModal().within(() => {
+            cy.get('[data-testid="issue-comment"]')
+                .first()
+                .contains('Edit')
+                .click()
+                .should('not.exist');
+
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', previousComment)
+                .clear()
+                .type(editComment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('contain', 'Edit')
+                .and('contain', editComment);
+        });
+
+        getIssueDetailsModal()
+            .find('[data-testid="issue-comment"]')
+            .contains('Delete')
+            .click();
+
+        cy.get('[data-testid="modal:confirm"]')
+            .contains('button', 'Delete comment')
+            .click()
+            .should('not.exist');
+
+        getIssueDetailsModal()
+            .find('[data-testid="issue-comment"]')
+            .contains(editComment)
+            .should('not.exist');
+
+    });
+   
 });
---
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -0,0 +1,174 @@
+import { faker } from '@faker-js/faker';
+
+describe('Time tracking', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
+      //Going straight to issue creation because existing issues are not good for assignment conditions
+      cy.visit(url + '/board?modal-issue-create=true');
+      });
+    });
+
+    const randomTitle = faker.lorem.word();
+    const randomDescription = faker.lorem.words(5);
+    const initialEstimateTime = 10;
+    const editEstimateTime = 20;
+    const timeSpent = 2;
+    const timeRemaining = 5;
+
+
+    it('Assignment 2. Time estimation: Add, edit, remove', () => {
+
+
+
+        //----------------------Begin creating a new issue to work with---------------------------------------
+        cy.get('[data-testid="modal:issue-create"]').within(() => {
+                    
+            cy.get('.ql-editor').type(randomDescription);
+            cy.get('input[name="title"]').type(randomTitle);
+
+            cy.get('[data-testid="form-field:reporterId"]').click();
+            cy.get('[data-testid="select-option:Pickle Rick"]').click();
+
+            cy.get('[data-testid="form-field:priority"]').click();
+            cy.get('[data-testid="select-option:Highest"]').click();
+
+            cy.get('button[type="submit"]').click();
+        });
+        //----------------------End creating a new issue-----------------------------------------------------
+
+
+        //----------------------Begin setting estimated time-------------------------------------------------
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('input[placeholder="Number"]').type(initialEstimateTime);
+
+        cy.get('[data-testid="icon:close"]').click();
+
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('input[placeholder="Number"]').should('have.value', initialEstimateTime);
+
+        cy.get('[data-testid="icon:close"]').click();
+
+
+        //----------------------End setting estimated time--------------------------------------------------
+
+
+        //----------------------Start updating estimated time-----------------------------------------------
+
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('input[placeholder="Number"]').clear().type(editEstimateTime);
+
+        cy.get('[data-testid="icon:close"]').click();
+
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('input[placeholder="Number"]').should('have.value', editEstimateTime);
+
+        cy.get('[data-testid="icon:close"]').click();
+
+        //----------------------End updating estimated time------------------------------------------------
+
+
+        //----------------------Start removing estimated time----------------------------------------------
+
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('input[placeholder="Number"]').clear();
+
+        cy.get('[data-testid="icon:close"]').click();
+
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('input[placeholder="Number"]').contains(editEstimateTime).should('not.exist');
+
+        //----------------------End removing estimated time------------------------------------------------
+
+    });
+
+    it.only('Assignment 2. Time Logging: Add, remove', () => {
+
+
+
+        //----------------------Begin creating a new issue to work with---------------------------------------
+        cy.get('[data-testid="modal:issue-create"]').within(() => {
+                    
+            cy.get('.ql-editor').type(randomDescription);
+            cy.get('input[name="title"]').type(randomTitle);
+
+            cy.get('[data-testid="form-field:reporterId"]').click();
+            cy.get('[data-testid="select-option:Pickle Rick"]').click();
+
+            cy.get('[data-testid="form-field:priority"]').click();
+            cy.get('[data-testid="select-option:Highest"]').click();
+
+            cy.get('button[type="submit"]').click();
+        });
+        //----------------------End creating a new issue-----------------------------------------------------
+
+
+
+        //----------------------Begin entering estimated time, logged time and remaining time----------------
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('input[placeholder="Number"]').type(initialEstimateTime);
+
+        cy.get('[data-testid="icon:stopwatch"]').click();
+        cy.get('[data-testid="modal:tracking"]').should('be.visible')
+            .within(() => {
+            cy.get('input[placeholder="Number"]').eq(0).type(timeSpent);
+            cy.get('input[placeholder="Number"]').eq(1).type(timeRemaining);
+            
+        cy.contains('button', 'Done').click();
+
+        });
+
+        cy.get('[data-testid="modal:issue-details"]').should('be.visible')
+        cy.contains("No time logged").should('not.exist')
+        cy.contains(`${timeSpent}${'h logged'}`).should('be.visible');
+        cy.contains(`${timeRemaining}${'h remaining'}`).should('be.visible');
+
+        cy.get('[data-testid="icon:close"]').click();
+
+
+        //----------------------End entering estimated time, logged time and remaining time------------------
+
+
+        //----------------------Begin removing logged time---------------------------------------------------
+
+        cy.get('[data-testid="list-issue"]');
+        cy.contains(randomTitle).click();
+
+        cy.get('[data-testid="icon:stopwatch"]').click();
+        cy.get('[data-testid="modal:tracking"]').should('be.visible')
+            .within(() => {
+            cy.get('input[placeholder="Number"]').eq(0).clear();
+            cy.get('input[placeholder="Number"]').eq(1).clear();
+            
+        cy.contains('button', 'Done').click();
+
+        });
+
+        cy.get('[data-testid="modal:issue-details"]').should('be.visible')
+
+        cy.contains("No time logged").should('be.visible')
+        cy.contains(`${timeSpent}${'h logged'}`).should('not.exist');
+        cy.contains(`${timeRemaining}${'h remaining'}`).should('not.exist');
+        
+        cy.contains(`${initialEstimateTime}${'h estimated'}`).should('be.visible');
+
+
+        //----------------------End removing logged time-----------------------------------------------------
+
+    });
+
+});
